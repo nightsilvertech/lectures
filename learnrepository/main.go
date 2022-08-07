898e66aefc3f1d50f4fe64606ee825f0bd4b7f28
muhammadisa <msawjk@gmail.com>
2022-08-07 15:50:31 +0700

    Fix

--- learnrepository/main.go
+++ learnrepository/main.go
@@ -15,6 +15,27 @@ type Order struct {
 	UpdatedAt time.Time
 }
 
+func GetOrderByID(db *sql.DB, orderId int64) (*Order, error) {
+	stmt, err := db.Prepare(`SELECT * FROM orders WHERE id = ?`)
+	if err != nil {
+		return nil, err
+	}
+
+	rows := stmt.QueryRow(orderId)
+	var order Order
+	err = rows.Scan(
+		&order.Id,
+		&order.Username,
+		&order.CreatedAt,
+		&order.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &order, nil
+}
+
 func GetOrders(db *sql.DB) ([]Order, error) {
 	stmt, err := db.Prepare(`SELECT * FROM orders`)
 	if err != nil {
@@ -56,12 +77,18 @@ func main() {
 		log.Fatal(err)
 	}
 
-	orders, err := GetOrders(db)
+	order, err := GetOrderByID(db, 5)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println(order)
 
-	for _, order := range orders {
-		fmt.Println(order)
-	}
+	//orders, err := GetOrders(db)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//for _, order := range orders {
+	//	fmt.Println(order)
+	//}
 }
